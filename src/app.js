178b775ded8ade6140955c533241aaa55fd4ae60
raccoonboy <raccoonboy0803@gmail.com>
2024-05-01 18:18:09 +0900

    Feat: canvas에 이미지 추가 기능

--- src/app.js
+++ src/app.js
@@ -5,13 +5,17 @@ const colorOption = [...document.querySelectorAll('.colorOption')];
 const modeBtn = document.querySelector('#modeBtn');
 const resetBtn = document.querySelector('#resetBtn');
 const eraseBtn = document.querySelector('#eraseBtn');
+const fileInput = document.querySelector('#file');
+const textInput = document.querySelector('#text');
 
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
+
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = 'round';
 
 let isPainting = false;
 let isFilling = false;
@@ -71,12 +75,36 @@ function onEraseClick() {
   ctx.lineWidth = lineWidth.value;
   ctx.stroke();
 }
+function onFileChange(e) {
+  console.dir(e);
+  const file = e.target.files[0];
+  const url = URL.createObjectURL(file);
+
+  const image = new Image();
+  image.src = url;
+  image.onload = () => {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  };
+}
+function onDoubleClick(e) {
+  const text = textInput.value;
+  if (text !== '') {
+    ctx.save(); // ctx의 현재 상태를 저장
+    // ctx.lineWidth = 1;
+    ctx.font = '48px serif';
+    ctx.fillText(text, e.offsetX, e.offsetY);
+    ctx.restore(); // 상태 저장값으로 복구
+  }
+}
 
 canvas.addEventListener('mousemove', onMove);
+// === canvas.onmousemove = onMove;
 canvas.addEventListener('mousedown', onMounseUpDown);
 canvas.addEventListener('mouseup', onMounseUpDown);
 canvas.addEventListener('mouseleave', onMounseUpDown);
 canvas.addEventListener('click', onCanvasClick);
+canvas.addEventListener('dblclick', onDoubleClick);
 
 lineWidth.addEventListener('change', onLineWidthChange);
 colorChange.addEventListener('change', onColorChange);
@@ -87,3 +115,4 @@ colorOption.forEach((color) => {
 modeBtn.addEventListener('click', onModeClick);
 resetBtn.addEventListener('click', onResetClick);
 eraseBtn.addEventListener('click', onEraseClick);
+fileInput.addEventListener('change', onFileChange);
